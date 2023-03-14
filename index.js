a64bca2cc1f1bfc5e75c5ed8a933f7c721bc9258
Sharon Tokiti Mshekero <sharontokiti@gmail.com>
2023-03-14 13:03:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply);
 
-// Write your code here
+const random = (Math.ceil (Math.random()* 10) +1)
+
+let num3= 10;
+let num4=6;
+const mod = num3 % num4;
+console.log(mod);
+
+ const max = Math.max(14,15,16,17,18,19,20)
